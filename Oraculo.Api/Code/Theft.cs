c97378f36fbe98a0075bf55cc0f838ef657683ff
erik <erik.fernandess@hotmail.com>
2022-06-28 23:13:42 -0300

    projeto api

---
+++ Oraculo.Api/Code/Theft.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Oraculo.Api.Code
+{
+    public class Theft
+    {
+        [Key]
+        string NumeroBO { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        string Type { get; set; }
+
+        [Required]
+        string TipoBO { get; set; }
+
+        [Required]
+        string Cidade { get; set; }
+
+        [Required]
+        string Delegacia { get; set; }
+
+        [Required]
+        string Elaboração { get; set; }
+
+        [Required]
+        string DataFato { get; set; }
+
+        [Required]
+        string DataRegistro { get; set; }
+
+        [Required]
+        string EndereçoFato { get; set; }
+    }
+}
---
+++ Oraculo.Api/Controllers/TheftController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Oraculo.Api.Code;
+
+namespace Oraculo.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TheftController : ControllerBase
+    {
+        [HttpGet(Name = "GetTheft")]
+        public IEnumerable<Theft> Get()
+        {
+          return  new List<Theft>();
+        }
+
+        //[HttpGet("{id}", Name = "GetTheft")]
+        //public IActionResult Get(int id)
+        //{
+        //    //var model = service.GetFilme(id);
+        //    //if (model == null)
+        //    //    return NotFound();
+
+        //    //var outputModel = ToOutputModel(model);
+        //    return Ok(outputModel);
+        //}
+    }
+}
